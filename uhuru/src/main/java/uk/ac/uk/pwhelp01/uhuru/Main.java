e5c6e80cd8cf94518919e8754446d4a2c1c1c6a9
peedeeboy <peedeeboy@BurningSpear>
2016-02-20 14:49:19 +0000

    Managed to build classes dynamically :)

---
+++ uhuru/src/main/java/uk/ac/uk/pwhelp01/uhuru/Main.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.uk.pwhelp01.uhuru;
+
+import java.util.Map;
+import org.apache.maven.cli.MavenCli;
+import org.xeustechnologies.jcl.JarClassLoader;
+import org.xeustechnologies.jcl.JclObjectFactory;
+
+/**
+ *
+ * @author peedeeboy
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        buildInnerProject();
+        loadClasses();
+        System.out.println("Got here");
+       
+    }
+    
+    
+    private static void buildInnerProject() {
+        
+         // Create new instance of MavenCLI
+        MavenCli cli = new MavenCli();
+        
+        // Build inner project
+        int result = cli.doMain(new String[]{"clean", "compile"}, 
+                "/home/peedeeboy/git/hesa-utility/hyperjaxb3-ejb-template-basic-0.5.6", 
+                System.out, System.out);
+           
+        // Print result
+        System.out.println("result: " + result);
+    }
+    
+    
+    private static void loadClasses() {
+        
+        JarClassLoader jcl = new JarClassLoader();   
+        jcl.add("/home/peedeeboy/git/hesa-utility/hyperjaxb3-ejb-template-basic-0.5.6/target/generated-sources/xjc/generated/"); //Load class folder   
+        
+        
+        JclObjectFactory factory = JclObjectFactory.getInstance();  
+        
+        Map<String,Class> loadedClasses = jcl.getLoadedClasses();
+        
+        for (Map.Entry<String,Class> entry : loadedClasses.entrySet()) {
+            System.out.println(entry.getValue().getClass().getCanonicalName());
+        }
+        
+        //Create object of loaded class  
+        //Object obj = factory.create(jcl,"generated.APStudentRecord");
+        //System.out.println(obj.getClass().getCanonicalName());
+        
+    }
+}
